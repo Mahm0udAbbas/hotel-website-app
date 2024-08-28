a29c088dc9878634e74a706fad14b26cf066e8f6
Mahmoud Abbas <mahmoudabbas393@gmail.com>
2024-08-28 09:00:11 +0300

    feat: add the cabin page with cabin details using dynamic routes

--- app/loading.js
+++ app/loading.js
@@ -1,5 +1,10 @@
 import React from "react";
+import Spinner from "./_components/Spinner";
 
 export default function Loading() {
-  return <div>loading...</div>;
+  return (
+    <div>
+      <Spinner />
+    </div>
+  );
 }
