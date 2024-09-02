c78a6a68a2e062155b729a36ffe31e72f1af11ef
Mahmoud Abbas <mahmoudabbas393@gmail.com>
2024-09-02 13:08:14 +0300

    feat: add update user data feature

--- app/_lib/actions.js
+++ app/_lib/actions.js
@@ -1,10 +1,31 @@
 "use server";
 
-import { signIn, signOut } from "./auth";
+import { revalidatePath } from "next/cache";
+import { auth, signIn, signOut } from "./auth";
+import { updateGuest } from "./data-service";
 
 export async function signInAction() {
   await signIn("google", { redirectTo: "/account" });
 }
 export async function signOutAction() {
   await signOut({ redirectTo: "/" });
+}
+
+export async function updateGuestAction(formData) {
+  const session = await auth();
+
+  if (!session) throw new Error("you must login to update");
+  const nationalID = formData.get("nationalID");
+  if (!/^[a-zA-Z0-9]{6,12}$/.test(nationalID))
+    throw new Error("enter a valid national Id ");
+
+  const [nationality, countryFlag] = formData.get("nationality").split("%");
+
+  const updatedGuest = {
+    nationalID,
+    nationality,
+    countryFlag,
+  };
+  await updateGuest(session.user.guestId, updatedGuest);
+  revalidatePath("account/profile");
 }
